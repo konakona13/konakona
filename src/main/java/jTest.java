8b8170abc50f67e06240f01c71cc3916c82e0a83
retyGot <retygot@gmail.com>
2019-08-16 10:32:38 +0900

    컨트롤오류 수정

--- src/main/java/jTest.java
+++ src/main/java/jTest.java
@@ -3,6 +3,22 @@ public class jTest
 {
 	int i;
 	int j;
+	public int getI()
+	{
+		return i;
+	}
+	public void setI(int i)
+	{
+		this.i = i;
+	}
+	public int getJ()
+	{
+		return j;
+	}
+	public void setJ(int j)
+	{
+		this.j = j;
+	}
 	
 
 }
